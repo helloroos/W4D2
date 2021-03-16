2f1be9a8055d9bdd6ebb7289b19ae62297094ea3
Daniel Cho <thedanielcho@gmail.com>
2021-03-16 15:27:51 -0400

    Built out board

--- chess/board.rb
+++ chess/board.rb
@@ -1,18 +1,34 @@
+require_relative "piece"
+require_relative "nullpiece"
 class Board
 
     def initialize
-        @rows = Array.new(8) { Array.new(8) }
+        @grid = Array.new(8) { Array.new(8) }
+        @grid.each_with_index do |row, idx1|
+            if idx1 == 0 || idx1 == 1 || idx1 = 6 || idx1 == 7
+                row.map! { |pos| Piece.new }
+            else
+                row.map! { |pos| NullPiece.new }
+            end
+        end
     end
 
     def [](pos)
-        
+        row = pos[0]
+        col = pos[1]
+        @grid[row][col]
     end
 
     def []=(pos, val)
+        row = pos[0]
+        col = pos[1]
+        @grid[row][col] = val
     end
 
     def move_piece(start_pos, end_pos)
     end
+
+
 
 end
 
@@ -26,3 +42,4 @@ end
 #     [0, 1, 2, 3, 4, 5, 6, 7], 6
 #     [0, 1, 2, 3, 4, 5, 6, 7]  7
 # ]
+
---
+++ chess/nullpiece.rb
@@ -0,0 +1,8 @@
+require_relative "piece"
+class NullPiece < Piece
+
+    def inspect
+        "N"
+    end
+
+end
