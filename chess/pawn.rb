4481858eabdddf3ac2c26218f383691498be0a21
Daniel Cho <thedanielcho@gmail.com>
2021-03-17 14:45:55 -0400

    Continue pawn

--- chess/pawn.rb
+++ chess/pawn.rb
@@ -8,7 +8,18 @@ class Pawn < Piece
     end
 
     def moves
+        final_moves = []
 
+        #logic
+        #if row[i] == 1 or 6
+            #shovel in forward_steps + forward_direction
+        #elseif if piece at self.pos + [1,1] or [1,-1]
+
+        #elsif if pos[+1] || pos[-1] == empty?(nullpiece)
+            #shovel in forward_direction
+
+
+        final_moves
     end
 
     private
@@ -35,8 +46,14 @@ class Pawn < Piece
     end
 
     def side_attacks
-        if color == :black
-            return [[1, 1], [1, -1]]
+        col, row = @pos #@pos = [4,5]
+        if color == :black && (@board[[col+1,row+1]].color == :white &&  @board[[col+1,row-1]].color == :white)
+            return [[1,1],[1,-1]]
+        elsif color == :black && @board[[col+1,row+1]].color == :white #[5,6]
+            return [[1, 1]]
+        elsif color == :black && @board[[col+1,row-1]].color == :white #[5,3]
+            return [[1, -1]]
+        end
     end
 
 end
